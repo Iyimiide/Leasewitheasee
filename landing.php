6e557091342e82dc96c57af1082c90b4d6e27c53
Iyimiide <124675039+Iyimiide@users.noreply.github.com>
2023-04-05 20:33:07 +0100

    Create landing.php

---
+++ landing.php
@@ -0,0 +1,95 @@
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Lease With Ease</title>
+          <style>
+/* CSS for logo */
+#logo {
+    height: 90px;
+    width: 180px;
+    float: left;
+}
+
+/* CSS for body */
+body {
+        background-image: url("Assets/landing.jpg");
+        background-size: cover;
+        background-position: center;
+        background-color: white;
+}
+
+/* CSS for header */
+header {
+  background-color: white;
+  color: Black;
+  padding: 20px;
+}
+
+/* CSS for logo */
+.logo {
+  font-size: 2em;
+  font-weight: bold;
+  text-transform: uppercase;
+}
+
+/* CSS for navigation menu */
+nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 10px;
+}
+
+nav ul {
+  list-style-type: none;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+nav li {
+  margin-right: 20px;
+}
+
+nav a {
+  color: Black;
+  text-decoration: none;
+  text-transform: uppercase;
+  font-weight: bold;
+}
+
+/* CSS for main content */
+main {
+  margin-top: 20px;
+}
+
+/* CSS for footer */
+footer {
+  background-color: grey;
+  color: white;
+  padding: 20px;
+  text-align: center;
+}
+        </style>
+     </head>
+    <body>
+      <header>
+        <img src="Assets/LeaseWithEase.png" alt="logo" id="logo">
+        <nav>
+              <ul>
+                  <li> <a href="index.php">Home</a></li>
+                  <li> <a href="logout.php">Logout/a></li>
+                  <li> <a href="index4.php">View listings</a></li>
+              </ul>
+          </nav>
+          </header>
+    <main>
+      <h1>Welcome to Lease With Ease</h1>
+      <h2>This is your subscriber account</h2>
+      <p> Your subscriber priviledges are:.... </p>
+    </main>
+    <footer>
+      <p>&copy; 2023 Lease With Ease. All rights reserved.</p>
+    </footer>
+  </body>
+</html>
