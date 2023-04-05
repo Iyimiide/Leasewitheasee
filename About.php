7085a44a6c0f91c40f332662104c54d0baf265b1
Iyimiide <124675039+Iyimiide@users.noreply.github.com>
2023-04-05 19:58:17 +0100

    Update About.php

--- About.php
+++ About.php
@@ -1,38 +1,95 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
+<<html lang="en">
+  <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About</title>
-    <link rel="stylesheet" href="css/about.css">
-</head>
-<body>
-    <img src="images/LeaseWithEase.png" alt="Lease with Ease" height="90px" width="90px">
-    <div class="navbar">
+    <title>LEASE WITH EASE</title>
+          <style>
+/* CSS for logo */
+#logo {
+    height: 90px;
+    width: 180px;
+    float: left;
+}
+
+/* CSS for body */
+body {
+        background-image: url("Assets/land.JPG");
+        background-size: cover;
+        background-position: center;
+        background-color: white;
+}
+
+/* CSS for header */
+header {
+  background-color: rgb(51, 61, 51);
+  color: white;
+  padding: 20px;
+}
+
+/* CSS for logo */
+.logo {
+  font-size: 2em;
+  font-weight: bold;
+  text-transform: uppercase;
+}
+
+/* CSS for navigation menu */
+nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 10px;
+}
+
+nav ul {
+  list-style-type: none;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+nav li {
+  margin-right: 20px;
+}
+
+nav a {
+  color: Black;
+  text-decoration: none;
+  text-transform: uppercase;
+  font-weight: bold;
+}
+
+/* CSS for main content */
+main {
+  margin-top: 20px;
+}
+
+/* CSS for footer */
+footer {
+  background-color: #333333;
+  color: white;
+  padding: 20px;
+  text-align: center;
+}
+        </style>
+     </head>
+    <body>
+      <header>
+        <img src="Assets/LeaseWithEase.png" alt="logo" id="logo">
         <nav>
-            <div class="right">
-                <div class="links">
-                <li> <a href="index.html">Home</a></li>
-                     <a href="registration.php">Signup Customer</a>
-                     <a href="login.php">Login Customer</a>
-                </div>
-            </div>
-            <div class="left">
-                <div class="serach">
-                     <input type="search" name="search" class="search_field" placeholder="search" />
-                </div>
-            </div>
-        </nav>
-    </div>
-  
-  <main>
+              <ul>
+                  <li> <a href="index.html">Home</a></li>
+                  <li> <a href="About.php">About Us</a></li>
+                  <li> <a href="login.php">Sign Up/Login</a></li>
+                  <li> <a href="About.php">Contact Us</a></li>
+                  <li> <a href="login2.php">Realtors Login/Signup</a></li>
+              </ul>
+          </nav>
+          </header>
+    <main>
     <section>
       <h1>About Us</h1>
       <p>At Lease With Ease, we understand that the process of leasing land can be a complicated and time-consuming process. That's why we've made it our mission to help individuals and businesses navigate the process with ease.
-
 Our team of experienced professionals has a deep understanding of the local real estate market and the legal requirements involved in leasing land. We work closely with our clients to understand their unique needs and provide tailored solutions to meet their specific requirements.
-
 Whether you're looking to lease land for commercial, residential, or agricultural purposes,Lease With Ease can help. We are committed to providing transparent, efficient, and cost-effective services that simplify the process and save our clients time and money.</p>
       <p>Since our founding in 2023, we have grown to become a national leader in the Real Estate industry. We have achieved this success through our commitment to quality, innovation, and customer service.</p>
       <p>Our team of dedicated professionals includes Realtors, lawyers, designers, engineers, and customer service representatives who work together to ensure that our customers receive the best possible experience when using our service.</p>
@@ -41,27 +98,25 @@ Whether you're looking to lease land for commercial, residential, or agricultura
     <section>
       <h2>Our History</h2>
       <p>Lease With Ease was founded in 2023 by a group of experienced real estate professionals who recognized the need for a simplified and transparent process for leasing land in Nigeria.
-
 In the early days, Lease With Ease operated as a small consultancy firm, providing advice and guidance to clients looking to lease land in the region. Over time, the company expanded its services to include land surveys, property valuation, legal compliance, and project management.
-
 As Lease With Ease continued to grow, the company developed a reputation for its expertise and customer-centric approach. Today, Lease With Ease is one of the leading land leasing companies in Kano, known for its commitment to transparency, efficiency, and exceptional customer service.
-
 Despite its success, Lease With Ease remains focused on its mission to help individuals and businesses navigate the complex process of land leasing in Kano with ease. We are committed to providing innovative and effective solutions to its clients' land leasing needs.</p>
     </section>
   </main>
-  
-  </form>
+        <footer>
+  <p>LeaseWithEase Nigeria</p>
+  <p>Xofiri 07404167666 | Bummi 09090000252 | Database 08036101842</p>
+  <hr>
+  <p>Follow us on:</p>
+  <div class="social-media-icons">
+    <a href="https://www.instagram.com/LeaseWithEase/" target="_blank"><i class="fab fa-instagram"></i></a>
+    <a href="https://www.facebook.com/LeaseWithEase/" target="_blank"><i class="fab fa-facebook-f"></i></a>
+  </div>
+  <p><a href="/about-us">About Us</a></p>
+  <p class="copyright">
+    &copy; 2023 LeaseWithEase Nigeria. All rights reserved.
+  </p>
+</footer>
 
-    </div>
-
-
-    <!-- footer -->
-
-    <div class="footer">
-         <div class="footer_top">
-                <p>&nbsp;</p>
-         </div>
-         
-    </div>
-</body>
+  </body>
 </html>
