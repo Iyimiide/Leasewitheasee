109b7eb99ba6745ff8e7a660bb04d6249c8d0bc1
Iyimiide <124675039+Iyimiide@users.noreply.github.com>
2023-04-05 19:26:43 +0100

    Add files via upload

---
+++ About.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+    <link rel="stylesheet" href="css/About.css">
+</head>
+<body>
+    <img src="images/LeaseWithEase.png" alt="Lease with Ease" height="90px" width="90px">
+    <div class="navbar">
+        <nav>
+            <div class="right">
+                <div class="links">
+                <li> <a href="index.html">Home</a></li>
+                     <a href="Services">Services</a>
+                     <a href="signupCustomer.html">Signup</a>
+                     <a href="loginCustomer.html">Login</a>
+                </div>
+            </div>
+            <div class="left">
+                <div class="serach">
+                     <input type="search" name="search" class="search_field" placeholder="search" />
+                </div>
+            </div>
+        </nav>
+    </div>
+  
+  <main>
+    <section>
+      <h1>About Us</h1>
+      <p>At Lease With Ease, we understand that the process of leasing land can be a complicated and time-consuming process. That's why we've made it our mission to help individuals and businesses navigate the process with ease.
+
+Our team of experienced professionals has a deep understanding of the local real estate market and the legal requirements involved in leasing land. We work closely with our clients to understand their unique needs and provide tailored solutions to meet their specific requirements.
+
+Whether you're looking to lease land for commercial, residential, or agricultural purposes,Lease With Ease can help. We are committed to providing transparent, efficient, and cost-effective services that simplify the process and save our clients time and money.</p>
+      <p>Since our founding in 2023, we have grown to become a national leader in the Real Estate industry. We have achieved this success through our commitment to quality, innovation, and customer service.</p>
+      <p>Our team of dedicated professionals includes Realtors, lawyers, designers, engineers, and customer service representatives who work together to ensure that our customers receive the best possible experience when using our service.</p>
+    </section>
+    
+    <section>
+      <h2>Our History</h2>
+      <p>Lease With Ease was founded in 2023 by a group of experienced real estate professionals who recognized the need for a simplified and transparent process for leasing land in Nigeria.
+
+In the early days, Lease With Ease operated as a small consultancy firm, providing advice and guidance to clients looking to lease land in the region. Over time, the company expanded its services to include land surveys, property valuation, legal compliance, and project management.
+
+As Lease With Ease continued to grow, the company developed a reputation for its expertise and customer-centric approach. Today, Lease With Ease is one of the leading land leasing companies in Kano, known for its commitment to transparency, efficiency, and exceptional customer service.
+
+Despite its success, Lease With Ease remains focused on its mission to help individuals and businesses navigate the complex process of land leasing in Kano with ease. We are committed to providing innovative and effective solutions to its clients' land leasing needs.</p>
+    </section>
+  </main>
+  
+  </form>
+
+    </div>
+
+
+    <!-- footer -->
+
+    <div class="footer">
+         <div class="footer_top">
+                <p>&nbsp;</p>
+         </div>
+         
+    </div>
+</body>
+</html>
---
+++ registration.php
@@ -0,0 +1,105 @@
+<?php
+require 'config.php';
+if(!empty($_SESSION["id"])){
+  header("Location: index.php");
+}
+if(isset($_POST["submit"])){
+  $name = $_POST["name"];
+  $username = $_POST["username"];
+  $email = $_POST["email"];
+  $password = $_POST["password"];
+  $confirmpassword = $_POST["confirmpassword"];
+  $duplicate = mysqli_query($conn, "SELECT * FROM tb_user WHERE username = '$username' OR email = '$email'");
+  if(mysqli_num_rows($duplicate) > 0){
+    echo "<script> alert('Username or Email Has Already Taken'); </script>";
+  }
+  else{
+    if($password == $confirmpassword){
+      $query = "INSERT INTO tb_user VALUES('','$name','$username','$email','$password')";
+      mysqli_query($conn, $query);
+      echo "<script> alert('Registration Successful'); </script>";
+    }
+    else{
+      echo "<script> alert('Password Does Not Match'); </script>";
+    }
+  }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Create Your Lease With Ease Account</title>
+    <style>
+      body {
+        font-family: Arial, sans-serif;
+        margin: 0;
+        padding: 0;
+      }
+
+      h2 {
+        margin-bottom: 20px;
+      }
+
+      form {
+        max-width: 500px;
+        margin: 0 auto;
+      }
+
+      label {
+        display: inline-block;
+        width: 150px;
+        margin-bottom: 10px;
+      }
+
+      input[type="text"], input[type="email"], input[type="password"] {
+        width: 250px;
+        padding: 5px;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        font-size: 16px;
+      }
+
+      button[type="submit"] {
+        padding: 10px;
+        background-color: #4CAF50;
+        color: #fff;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+        font-size: 16px;
+      }
+
+      button[type="submit"]:hover {
+        background-color: #3e8e41;
+      }
+
+      a {
+        display: block;
+        margin-top: 20px;
+        text-align: center;
+        color: #333;
+        text-decoration: none;
+      }
+    </style>
+  </head>
+  <body>
+    <h2>Create Your Lease With Ease Account</h2>
+    <form class="" action="" method="post" autocomplete="off">
+      <label for="name">Name:</label>
+      <input type="text" name="name" id="name" required value=""> <br>
+      <label for="username">Username:</label>
+      <input type="text" name="username" id="username" required value=""> <br>
+      <label for="email">Email:</label>
+      <input type="email" name="email" id="email" required value=""> <br>
+      <label for="password">Password:</label>
+      <input type="password" name="password" id="password" required value=""> <br>
+      <label for="confirmpassword">Confirm Password:</label>
+      <input type="password" name="confirmpassword" id="confirmpassword" required value=""> <br>
+      <button type="submit" name="submit">Register</button>
+   </form>
+    <br>
+    <a href="login.php">Login</a>
+  </body>
+</html>
