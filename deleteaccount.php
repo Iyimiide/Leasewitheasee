e695fbc9be9f5ecc55d2b77965b1fdecee71be58
Iyimiide <124675039+Iyimiide@users.noreply.github.com>
2023-04-11 08:31:36 +0100

    Create deleteaccount.php

---
+++ deleteaccount.php
@@ -0,0 +1,95 @@
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Lease With Ease</title>
+          <style>
+/* CSS for logo */
+#logo {
+    height: 90px;
+    width: 180px;
+    float: left;
+}
+
+/* CSS for body */
+body {
+        background-image: url("Assets/delete.jpeg");
+        background-size: cover;
+        background-position: center;
+        background-color: white;
+}
+
+/* CSS for header */
+header {
+  background-color: white;
+  color: Black;
+  padding: 20px;
+}
+
+/* CSS for logo */
+.logo {
+  font-size: 2em;
+  font-weight: bold;
+  text-transform: uppercase;
+}
+
+/* CSS for navigation menu */
+nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 10px;
+}
+
+nav ul {
+  list-style-type: none;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+nav li {
+  margin-right: 20px;
+}
+
+nav a {
+  color: Black;
+  text-decoration: none;
+  text-transform: uppercase;
+  font-weight: bold;
+}
+
+/* CSS for main content */
+main {
+  margin-top: 20px;
+}
+
+/* CSS for footer */
+footer {
+  background-color: grey;
+  color: white;
+  padding: 20px;
+  text-align: center;
+}
+        </style>
+     </head>
+    <body>
+      <header>
+        <img src="Assets/LeaseWithEase.png" alt="logo" id="logo">
+        <nav>
+              <ul>
+                  <li> <a href="index.php">Home</a></li>
+                  <li> <a href="logout.php">Logout/a></li>
+                  <li> <a href="index3.php">Add listings</a></li>
+              </ul>
+          </nav>
+          </header>
+    <main>
+      <h1>We are sad to see you go</h1>
+      <h2>If you'd like to proceed with deleting your account, kindly send an email to admin@leasewithease.com and we'd be sure to erase all your data from our records within 2 hours.</h2>
+      <p> This is because we are required by law to carry out quick checks in line with fraud prevention </p>
+    </main>
+    <footer>
+      <p>&copy; 2023 Lease With Ease. All rights reserved.</p>
+    </footer>
+  </body>
+</html>
